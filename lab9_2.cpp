a57570864fdd6bc6a439582652f868ab7d92a4da
NappoN49 <parawin39228@gmail.com>
2025-12-19 14:46:48 +0700

    lab9_2 complete

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int M,int N){
+    if(M<=0||N<=0){
+        cout<<"Invalid input";
+        return;
+    }
+    
+    for(int x=1;x<=M;x++){
+        for(int y=1;y<=N;y++) cout<<'O';
+        cout<<"\n";
+    }
+}
 
 int main(){
 	
